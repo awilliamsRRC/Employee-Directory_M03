298d3e6c6fa76df34c1034e2f66c9752760cb6d9
rrc_F2024FA_awilliams <awilliams3@academic.rrc.ca>
2025-02-20 11:38:40 -0600

    Added more parameters to employee validation schema

--- src/api/v1/schemas/employeeSchema.ts
+++ src/api/v1/schemas/employeeSchema.ts
@@ -1,6 +1,13 @@
 import Joi, { ObjectSchema } from 'joi';
 
 export const employeeSchema: ObjectSchema = Joi.object({
+    id:Joi.string().min(3).max(50).required().messages({
+        'string.base': 'id should be a type string',
+        'string.min': 'id should have at least 3 charcters',
+        'string.max': 'id should at most 50 characters',
+        'any.required': 'id is required'
+    }),
+
     name: Joi.string().min(3).max(50).required().messages({
         'string.base': 'Name should be a type string',
         'string.min': 'Name should have at least 3 charcters',
@@ -13,12 +20,22 @@ export const employeeSchema: ObjectSchema = Joi.object({
         'string.max': 'Position should at most 50 characters',
         'any.required': 'Position is required'
     }),
+    department:Joi.string().min(3).max(50).required().messages({
+        'string.base': 'department should be a type string',
+        'string.min': 'department should have at least 3 charcters',
+        'string.max': 'department should at most 50 characters',
+        'any.required': 'department is required'
+    }),
     email: Joi.string().min(3).max(50).required().messages({
         'string.base': 'E-mail should be a type string',
         'string.min': 'E-mail should have at least 3 charcters',
         'string.max': 'E-mail should at most 50 characters',
         'any.required': 'E-mail is required'
     }),
+    phone: Joi.string().pattern(/^\+?[1-9]\d{1,14}$/).optional().messages({
+        'string.base': 'Phone should be a type of string',
+        'string.pattern.base': 'Phone number must be a valid format (e.g., +1234567890)'
+        }),
     branchId: Joi.string().min(3).max(50).required().messages({
         'string.base': 'Branch ID should be a type string',
         'string.min': 'Branch ID should have at least 3 charcters',
