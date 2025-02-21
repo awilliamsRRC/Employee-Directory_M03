a2fa1e5a1c4f621579506c88a1af9c3385922a1b
rrc_F2024FA_awilliams <awilliams3@academic.rrc.ca>
2025-02-21 09:48:37 -0600

    Added error handling for controller,validation and routes.

--- src/api/v1/errors/errors.ts
+++ src/api/v1/errors/errors.ts
@@ -52,3 +52,79 @@ export class ServiceError extends Error {
         this.statusCode = statusCode;
     }
 }
+
+/**
+ * Class representing a controller error.
+ * Extends the built-in Error class to include an error code.
+ */
+export class ControlError extends Error {
+    code: string;
+    statusCode: number;
+
+    /**
+     * Creates a new ControlError instance.
+     * @param {string} message - The error message.
+     * @param {string} code - The error code.
+     * @param {number} code - The the http response code.
+     */
+    constructor(
+        message: string,
+        code: string,
+        statusCode: number = HTTP_STATUS.INTERNAL_SERVER_ERROR
+    ) {
+        super(message);
+        this.name = "ControlError";
+        this.code = code;
+        this.statusCode = statusCode;
+    }
+}
+/**
+ * Class representing a Router error.
+ * Extends the built-in Error class to include an error code.
+ */
+export class RouteError extends Error {
+    code: string;
+    statusCode: number;
+
+    /**
+     * Creates a new RouteError instance.
+     * @param {string} message - The error message.
+     * @param {string} code - The error code.
+     * @param {number} code - The the http response code.
+     */
+    constructor(
+        message: string,
+        code: string,
+        statusCode: number = HTTP_STATUS.INTERNAL_SERVER_ERROR
+    ) {
+        super(message);
+        this.name = "RouteError";
+        this.code = code;
+        this.statusCode = statusCode;
+    }
+}
+/**
+ * Class representing a validation error.
+ * Extends the built-in Error class to include an error code.
+ */
+export class ValidationError extends Error {
+    code: string;
+    statusCode: number;
+
+    /**
+     * Creates a new ValidationError instance.
+     * @param {string} message - The error message.
+     * @param {string} code - The error code.
+     * @param {number} code - The the http response code.
+     */
+    constructor(
+        message: string,
+        code: string,
+        statusCode: number = HTTP_STATUS.INTERNAL_SERVER_ERROR
+    ) {
+        super(message);
+        this.name = "ValidationError";
+        this.code = code;
+        this.statusCode = statusCode;
+    }
+}
