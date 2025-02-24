c4c06478af0f1e8b26f990d07ebdefc89b003220
rrc_F2024FA_awilliams <awilliams3@academic.rrc.ca>
2025-02-23 22:34:40 -0600

    trying to fix secret error 2

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,11 +1,12 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
-//import * as serviceAccount from "../branch-database-project3-firebase-adminsdk-fbsvc-4305f5893f.json";
+import * as serviceAccount from "../branch-database-project3-firebase-adminsdk-fbsvc-4305f5893f.json";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') || '{}');
+//const serviceAccount = JSON.parse(process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') || '{}');
 
 // Initialize the Firebase app with the service account credentials
 // This step is necessary before you can use any Firebase services
+console.log(serviceAccount);
 initializeApp({
 	credential: cert(serviceAccount as ServiceAccount),
 });
